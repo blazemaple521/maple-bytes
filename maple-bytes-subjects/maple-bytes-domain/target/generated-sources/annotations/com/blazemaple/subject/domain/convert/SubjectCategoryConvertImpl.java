152c653458a82ff27c6905efa02fec3b0dc53508
BlazeMaple <193237147@qq.com>
2024-01-21 15:02:40 +0800

    分类模块开发

---
+++ maple-bytes-subjects/maple-bytes-domain/target/generated-sources/annotations/com/blazemaple/subject/domain/convert/SubjectCategoryConvertImpl.java
@@ -0,0 +1,62 @@
+package com.blazemaple.subject.domain.convert;
+
+import com.blazemaple.subject.domain.entity.SubjectCategoryBO;
+import com.blazemaple.subject.infra.basic.entity.SubjectCategory;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-01-21T15:01:14+0800",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.8 (Oracle Corporation)"
+)
+public class SubjectCategoryConvertImpl implements SubjectCategoryConvert {
+
+    @Override
+    public SubjectCategory convertBoToCategory(SubjectCategoryBO subjectCategoryBO) {
+        if ( subjectCategoryBO == null ) {
+            return null;
+        }
+
+        SubjectCategory subjectCategory = new SubjectCategory();
+
+        subjectCategory.setId( subjectCategoryBO.getId() );
+        subjectCategory.setCategoryName( subjectCategoryBO.getCategoryName() );
+        subjectCategory.setCategoryType( subjectCategoryBO.getCategoryType() );
+        subjectCategory.setImageUrl( subjectCategoryBO.getImageUrl() );
+        subjectCategory.setParentId( subjectCategoryBO.getParentId() );
+
+        return subjectCategory;
+    }
+
+    @Override
+    public List<SubjectCategoryBO> convertToCategoryBoList(List<SubjectCategory> categoryList) {
+        if ( categoryList == null ) {
+            return null;
+        }
+
+        List<SubjectCategoryBO> list = new ArrayList<SubjectCategoryBO>( categoryList.size() );
+        for ( SubjectCategory subjectCategory : categoryList ) {
+            list.add( subjectCategoryToSubjectCategoryBO( subjectCategory ) );
+        }
+
+        return list;
+    }
+
+    protected SubjectCategoryBO subjectCategoryToSubjectCategoryBO(SubjectCategory subjectCategory) {
+        if ( subjectCategory == null ) {
+            return null;
+        }
+
+        SubjectCategoryBO subjectCategoryBO = new SubjectCategoryBO();
+
+        subjectCategoryBO.setId( subjectCategory.getId() );
+        subjectCategoryBO.setCategoryName( subjectCategory.getCategoryName() );
+        subjectCategoryBO.setCategoryType( subjectCategory.getCategoryType() );
+        subjectCategoryBO.setImageUrl( subjectCategory.getImageUrl() );
+        subjectCategoryBO.setParentId( subjectCategory.getParentId() );
+
+        return subjectCategoryBO;
+    }
+}
